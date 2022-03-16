32d2a39a632c95703f030f5b90a0dc5737150c4d
jirui.deng <dengjirui@thinkenergy.net.cn>
2022-03-16 18:32:11 +0800

    链表翻转方法2

---
+++ src/com/jirui/likou/toNum/JavaDemoP.java
@@ -0,0 +1,36 @@
+package com.jirui.likou.toNum;
+
+import org.junit.Test;
+
+/**
+ * @author jack Deng
+ * @version 1.0
+ * @date 2022/3/16 17:14
+ */
+public class JavaDemoP {
+    public static void main(String[] args) {
+        long l = myAtoi("   -42");
+        System.out.println(l);
+    }
+    public static int myAtoi(String s) {
+        String str = s.trim();
+
+        long ans = 0;
+        // 判断数字正负
+        boolean positionNumber = true;
+        for (int i=0;i<str.length();i++){
+            char a = str.charAt(i);
+            if(i==0 && (a=='-' || a =='+'))
+            {
+                positionNumber = a != '-';
+            }
+            else if(Character.isDigit(str.charAt(i))) {
+                ans = ans*10 + a - '0';
+                ans = positionNumber ? Math.min(ans, Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+            }
+            else
+                break;
+        }
+        return (int) (positionNumber? ans: -ans);
+    }
+}
